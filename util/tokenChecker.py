f8cc9f1d47117fcc40e276d7dc006a39fa97f942
TheBoss313 <vlad.wpg@gmail.com>
2022-01-29 22:20:40 -0500

    Improved the mass token verification process to save valid tokens to a separate file

--- util/tokenChecker.py
+++ util/tokenChecker.py
@@ -1,4 +1,5 @@
 import requests
+from tqdm import tqdm
 from colorama import Fore
 
 from util.plugins.common import getheaders
@@ -8,21 +9,27 @@ def validateToken(token):
     # contact discord api and see if you can get a valid response with the given token
     r = requests.get('https://discord.com/api/v9/users/@me', headers=getheaders(token))
     if r.status_code == 200:
-        # it is a valid token
-        print(f"\n{Fore.RED}Valid token : {token}{Fore.RESET}")
         return True
     else:
         return False
 
 
-def checkTokens(filename):
+def checkTokens(filename: str, newFile: bool=False, newFileName: str="tokensVALID.txt"):
+    if newFile:
+        newFileFile = open(newFileName, "a")
     valid = False
     with open(filename, "r") as f:
         lines = f.readlines()
-    for i in lines:
+    for i in tqdm(lines):
         i = i.replace("\n", "")
         if validateToken(i):
             valid = True
+            if newFile:
+                newFileFile.write(i)
+            else:
+                print(f"\n{Fore.RED}Valid token : {i}{Fore.RESET}")
     if not valid:
         print(f"{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}No Valid Tokens Found{Fore.RED}")
     input(f'{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}Enter anything to continue . . .  {Fore.RED}')
+    if newFile:
+        newFileFile.close()
